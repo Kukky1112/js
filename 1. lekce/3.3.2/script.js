bef3eb33ab6e8633f3b2b4d1ed55be8201273240
Kukky1112 <janekslama1111@gmail.com>
2026-02-11 11:49:31 +0100

    proměnné
    Aritmetické operace a cvičení na proměnné např. let, console.log....

--- 1. lekce/3.3.2/script.js
+++ 1. lekce/3.3.2/script.js
@@ -19,3 +19,41 @@ let a;
 console.log(a);
 let b = null;
 console.log(b)
+
+let isLockedIn = false;
+console.log (typeof isLockedIn)
+
+// Aritmetické operace
+
+let cisloA = 10;
+let cisloB = 5;
+console.log (cisloA + cisloB);
+console.log (cisloA - cisloB);
+console.log (cisloA / cisloB);
+console.log (cisloA * cisloB);
+
+// umocnování
+
+console.log(cisloA ** cisloB);
+
+//zbytek - modulo
+
+console.log(cisloA % cisloB);
+
+console.log(cisloA + cisloB * cisloB)
+
+console.log((cisloA + cisloB) * cisloB)
+
+// aktualizace proměnné (přiřazovací oparátory)
+
+let number = 18;
+number = number +1;
+number += 1;
+number -= 2;
+number *= 4;
+number /= 6;
+number %= 3;
+number++;
+number--;
+
+console.log(number)
---
+++ 1. lekce/cvičení 3.2.2/script.js
@@ -0,0 +1,9 @@
+let jmeno1 = "Lukáš";
+let jmeno2 = "Pepa";
+
+let věkLukáš = 48;
+let věkPepa = 4;
+
+let součetvěků = (věkLukáš + věkPepa)
+
+console.log(jmeno1 + " a " + jmeno2 + " jsou dohromady staří " + součetvěků + " let ")
