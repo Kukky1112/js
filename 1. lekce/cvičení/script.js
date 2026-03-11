a3cdc2b9472b940946e8872b5868edde1c3575c5
Kukky1112 <janekslama1111@gmail.com>
2026-03-11 11:19:47 +0100

    shrnutí všech cvičení do odkazů

--- 1. lekce/cvičení/script.js
+++ 1. lekce/cvičení/script.js
@@ -1,9 +1,9 @@
-let totalprice = 80;
+let totalprice = 50;
 let discountedprice = 0.7
 let finalprice = (totalprice * discountedprice)
 
 if (totalprice > 100) {
-console.log("Gratuluji! Cena se vám snížíla na" + finalprice + "kč")
+console.log("Gratuluji! Cena se vám snížíla na" + finalprice + " kč ")
 } else { 
     console.log(" Máš nákup těsně pod 100 kč. Zkus ještě nakoupit zboží za minimálně " + (100 - totalprice) + " kč ")
 }
