583cc87002824884a02251e9e8c1862f1104d551
Kukky1112 <janekslama1111@gmail.com>
2026-03-18 10:58:28 +0100

    Dokončení úkolu

--- 3.3.4/script.js
+++ 3.3.4/script.js
@@ -1,15 +1,32 @@
 let isLoggedIn = true;
 let isAdmin = false;
-
+ 
 if (isLoggedIn) {
-  console.log("Vítej na stránce!");
-
-  if (isAdmin) {
-    console.log("Přístup do admin panelu povolen.");
-  } else {
-    console.log("Přístup do uživatelského dashboardu povolen.");
-  }
-
-} else {
-  console.log("Nejsi přihlášený/á. Přihlas se.");
+ console.log("Vítejte na stránce!")
+ if (isAdmin = true) {
+ console.log("Přístup do panelu admin povolen")
+ } else {
+ console.log("Přístup jako uživatel povolen!")
+ }
 }
+ 
+// Operátory
+ 
+let age = 20;
+let hasDriverLicense = true;
+ 
+if (age >= 18 && hasDriverLicense) {
+ console.log("Můžeš řídit");
+} else {
+ console.log("Řídit můžeš")
+}
+ 
+let isHoliday = false;
+let isWeekend = true;
+ 
+if (isHoliday || isWeekend) {
+ console.log("Je čas relaxovat")
+} else {
+ console.log("Musíme pracovat")
+}
+ 
